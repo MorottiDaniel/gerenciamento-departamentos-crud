c76d84b120294acba4237e8ce93e4e4393cb0efb
Daniel Morotti Vieira <morottivieirad@gmail.com>
2026-03-24 09:12:28 -0300

    Entity

---
+++ src/main/java/com/morotti/departamentos/DepartamentosApplication.java
@@ -0,0 +1,13 @@
+package com.morotti.departamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartamentosApplication.class, args);
+	}
+
+}
